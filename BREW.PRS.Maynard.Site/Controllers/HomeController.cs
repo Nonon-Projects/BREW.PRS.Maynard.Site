944d5df7032c7a8d4f0db4500ff34d97cc495849
Alpe Nino Gulay <alpe-nino.gulay@viseo.com>
2020-07-25 15:26:26 +0800

    Add tracking

--- BREW.PRS.Maynard.Site/Controllers/HomeController.cs
+++ BREW.PRS.Maynard.Site/Controllers/HomeController.cs
@@ -1,6 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,11 +13,60 @@ namespace BREW.PRS.Maynard.Site.Controllers
 {
     public class HomeController : Controller
     {
+        [HttpGet]
         public ActionResult Index()
         {
+            var data = Request.Headers;
+            var bo = Request.Form;
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(LocatorDto locatorDto)
+        {
+ 
+            return View();
+        }
+        [HttpPost]
+        public async Task<ActionResult> MyPost(LocatorDto locatorDto)
+        {
+            var client = new HttpClient();
+            await client.PostAsync("https://dominoapi.azurewebsites.net/api/Locators/Ensure/", locatorDto, new JsonMediaTypeFormatter());
+            // var response = await client.PostAsync("https://localhost:44300/api/Locators/Ensure/", locatorDto, new JsonMediaTypeFormatter());
+
+            // var json = JsonConvert.SerializeObject(locatorDto);
+            // var data = new StringContent(json, Encoding.UTF8, "application/json");
+            //using (var client = new HttpClient())  
+            //{   
+            //    client.BaseAddress = new Uri("http://localhost:44300/api/Locators/Ensure");
+            //    try
+            //    {
+            //        var response = await client.PostAsync("", data);
+            //        var aa = "";
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        var a = "";
+            //    }
+            //}
+            //using (var client = new HttpClient())
+            //{
+            //    client.BaseAddress = new Uri("http://localhost:44300/api/Locators/Ensure");
+
+            //    //HTTP POST
+            //    var response = await client.PostAsync(uri, stringContent);
+            //   // var postTask = client.PostAsync
+            //   // postTask.Wait();
+
+            //    var result = postTask.Result;
+            //    if (result.IsSuccessStatusCode)
+            //    {
+            //        return RedirectToAction("Index");
+            //    }
+            //}
+
+            return View("About");
+        }
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -26,5 +80,16 @@ namespace BREW.PRS.Maynard.Site.Controllers
 
             return View();
         }
+    }
+    public class LocatorDto 
+    {
+        public string Ip { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public string HostName { get; set; }
+        public string Location { get; set; }
+        public string Org { get; set; }
+        public string Postal { get; set; }
+        public string Region { get; set; }
     }
 }
